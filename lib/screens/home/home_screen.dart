a310a5a9037765d4defbd64d701a3768d951500f
Sharifjon <sharifboymuminov@gmail.com>
2024-06-13 19:51:03 +0500

    norm

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:foydali_nuqtalar/screens/home/widget/my_drawer.dart';
-import 'package:foydali_nuqtalar/screens/widget/global_button.dart';
-import 'package:foydali_nuqtalar/utils/app_colors.dart';
 import 'package:foydali_nuqtalar/utils/app_images.dart';
 import 'package:foydali_nuqtalar/utils/app_size.dart';
-import 'package:foydali_nuqtalar/utils/app_text_style.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
